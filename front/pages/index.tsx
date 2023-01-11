71d89aeecf6437eed34c885ce78b7366f040bf6d
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-11 15:20:17 +0900

    feat : 인덱스

---
+++ front/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Applayout from '../componets/Applayout'
+
+const Index = () => {
+  return (
+    <Applayout>
+      <div>hello</div>
+    </Applayout>
+  )
+}
+
+export default Index
