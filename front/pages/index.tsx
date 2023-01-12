fbebd7a85840b1b1a57d97d1d42c1e81e9b00854
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-12 13:32:10 +0900

    feat(1/11) : 인덱스 수정

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Applayout from '../componets/Applayout'
+import PostForm from '../componets/PostForm'
+import PostCard from '../componets/PostCard'
+import 'antd/dist/antd.css';
 
 const Index = () => {
+  const isLoign = useSelector((state) => state.user.isLogin);
+  const mainPosts = useSelector((state) => state.post.mainPosts)
   return (
     <Applayout>
-      <div>hello</div>
+      {isLoign && <PostForm />}
+      {mainPosts.map((post) => <PostCard key={post.id} post={post} />)}
     </Applayout>
   )
 }
