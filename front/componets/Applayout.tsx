28d6f27525af10f885b55638f2002f3e6e3f9b63
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-12 13:29:19 +0900

    feat(1/11) : 레이아웃 변경

--- front/componets/Applayout.tsx
+++ front/componets/Applayout.tsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import { Input, Menu, Row, Col } from 'antd'
 import UserProfile from './UserProfile'
 import LoginForm from './LoginForm'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import { useSelector } from 'react-redux'
+
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+
+  .ant-col:first-child {
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `
 
 export default function Applayout({ children }) {
-  const [isLogin, setIsLogin] = useState(false)
-
-  const aa = () => {
-    setIsLogin(true)
-  }
+  const isLogin = useSelector((state) => state.user.isLogin)
   
   return (
     <div>
+      <Global />
       <Menu mode='horizontal'>
         <Menu.Item>
           <Link href='/'><a>노드버드</a></Link>
@@ -35,7 +47,7 @@ export default function Applayout({ children }) {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLogin ? <UserProfile setIsLogin={setIsLogin}/> : <LoginForm setIsLogin={setIsLogin}/>}
+          {isLogin ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
         {children}
