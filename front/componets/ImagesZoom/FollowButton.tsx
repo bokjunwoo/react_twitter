2dbcf8324c6120b896ee7fd8323e8351a219e2eb
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-15 23:09:23 +0900

    feat(1/15) : 팔로우 버튼 추가

---
+++ front/componets/ImagesZoom/FollowButton.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'antd';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { FOLLOW_REQUEST, UNFOLLOW_REQUEST } from '../../reducers/user';
+
+export default function FollowButton({ post }) {
+  const dispatch = useDispatch();
+
+  const { user, followLoading, unfollowLoading } = useSelector(
+    (state) => state.user
+  );
+
+  const isFollowing = user?.Followings.find((v) => v.id === post.User.id);
+
+  const onClickButton = useCallback(() => {
+    if (isFollowing) {
+      dispatch({
+        type: UNFOLLOW_REQUEST,
+        data: post.User.id,
+      });
+    } else {
+      dispatch({
+        type: FOLLOW_REQUEST,
+        data: post.User.id,
+      })
+    }
+  }, [isFollowing]);
+
+  return (
+    <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
+      {isFollowing ? '언팔로우' : '팔로우'}
+    </Button>
+  );
+}
+
+FollowButton.prototype = {
+  post: PropTypes.object.isRequired,
+};
