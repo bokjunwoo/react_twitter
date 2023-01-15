72f547102cf6a321baf06cbcb72c2891ed6a5e14
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-15 23:10:10 +0900

    feat(1/15) : 로그인폼 로딩 모션 추가

--- front/componets/LoginForm.tsx
+++ front/componets/LoginForm.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import userInput from '../hooks/userInput'
 import { useDispatch } from 'react-redux'
-import { loginAction } from '../reducers/user'
+import { loginRequestAction } from '../reducers/user'
+import { useSelector } from 'react-redux'
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -14,22 +15,23 @@ const FormWrapper = styled(Form)`
 `
 
 export default function LoginForm(){
+  const { logInLoading } = useSelector((state) => state.user)
   const dispatch = useDispatch();
-  const [id, onChangeId] = userInput('')
+  
+  const [email, onChangeEmail] = userInput('')
   const [pw, onChangePw] = userInput('')
 
   const onSubmitForm = useCallback((e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    console.log(id, pw);
-    dispatch(loginAction({ id, pw }))
-  }, [id, pw])
+    console.log(email, pw);
+    dispatch(loginRequestAction({ email, pw }))
+  }, [email, pw])
   
   return (
-    <FormWrapper>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
-        <label htmlFor='user_id'>아이디</label>
+        <label htmlFor='user_id'>이메일</label>
         <br />
-        <Input name='user_id' value={id} onChange={onChangeId} required />
+        <Input name='user_id' type='email' value={email} onChange={onChangeEmail} required />
       </div>
       <div>
         <label htmlFor='user_pw'>비밀번호</label>
@@ -37,7 +39,7 @@ export default function LoginForm(){
         <Input name='user_pw' value={pw} onChange={onChangePw} required />
       </div>
       <ButtonWrapper>
-        <Button type='primary' htmlType='submit' onClick={onSubmitForm}>로그인</Button>
+        <Button type='primary' htmlType='submit' loading={logInLoading}>로그인</Button>
         <Link href='/signup' ><a><Button>회원가입</Button></a></Link>
       </ButtonWrapper>
     </FormWrapper>
