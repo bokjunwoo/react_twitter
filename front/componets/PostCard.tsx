e3a77b445f73c4925e38c9503a78b3ed49986894
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-16 23:26:27 +0900

    feat(1/16) : 포스트카드 기능 추가

--- front/componets/PostCard.tsx
+++ front/componets/PostCard.tsx
@@ -1,24 +1,43 @@
-import { EllipsisOutlined, HeartOutlined, HeartTwoTone, MessageOutlined, RetweetOutlined } from '@ant-design/icons'
-import { Avatar, Comment, Button, Card, Image, List, Popover } from 'antd'
-import React, { useCallback, useState } from 'react'
-import { useSelector } from 'react-redux'
-import PropTypes from 'prop-types'
-import PostImages from './PostImages'
-import CommentForm from './CommentForm'
-import PostCardContent from './PostCardContent'
+import {
+  EllipsisOutlined,
+  HeartOutlined,
+  HeartTwoTone,
+  MessageOutlined,
+  RetweetOutlined,
+} from '@ant-design/icons';
+import { Avatar, Comment, Button, Card, Image, List, Popover } from 'antd';
+import React, { useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import PostImages from './PostImages';
+import CommentForm from './CommentForm';
+import PostCardContent from './PostCardContent';
+import { useDispatch } from 'react-redux';
+import { REMOVE_POST_REQUEST } from '../reducers/post';
+import FollowButton from './ImagesZoom/FollowButton';
 
 export default function PostCard({ post }) {
-  const user = useSelector((state) => state.user.user?.id)
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user.user?.id);
+  const { removePostLoading } = useSelector((state) => state.post);
 
   const [liked, setLiked] = useState(false);
   const onToggleLike = useCallback(() => {
-    setLiked((prev) => !prev)
+    setLiked((prev) => !prev);
   }, []);
 
   const [commentFormOpened, setCommentFormOpened] = useState(false);
   const onToggleComment = useCallback(() => {
-    setCommentFormOpened((prev) => !prev)
-  }, [])
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
+  const onRemovePost = useCallback(() => {
+    dispatch({
+      type: REMOVE_POST_REQUEST,
+      data: post.id,
+    });
+  }, []);
 
   return (
     <div style={{ marginBottom: 20 }}>
@@ -26,20 +45,41 @@ export default function PostCard({ post }) {
         cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key='retweet' />,
-          liked ? <HeartTwoTone key='heart' onClick={onToggleLike} twoToneColor='#eb2f96' /> : <HeartOutlined key='heart' onClick={onToggleLike}/>,
-          <MessageOutlined key='comment' onClick={onToggleComment}/>,
-          <Popover key='more' content={(
-            <Button.Group>
-              {user && post.User.id === user ?
-              <>
-                <Button>수정</Button>
-                <Button type='danger'>삭제</Button>
-              </> : <Button>신고</Button>}
-            </Button.Group>
-          )} >
+          liked ? (
+            <HeartTwoTone
+              key='heart'
+              onClick={onToggleLike}
+              twoToneColor='#eb2f96'
+            />
+          ) : (
+            <HeartOutlined key='heart' onClick={onToggleLike} />
+          ),
+          <MessageOutlined key='comment' onClick={onToggleComment} />,
+          <Popover
+            key='more'
+            content={
+              <Button.Group>
+                {user && post.User.id === user ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button
+                      type='danger'
+                      loading={removePostLoading}
+                      onClick={onRemovePost}
+                    >
+                      삭제
+                    </Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
             <EllipsisOutlined key='' />
-          </Popover>
+          </Popover>,
         ]}
+        extra={user && <FollowButton post={post} />}
       >
         <Image />
         <Card.Meta
@@ -50,14 +90,15 @@ export default function PostCard({ post }) {
       </Card>
       {commentFormOpened && (
         <div>
-          <CommentForm post={post}/>
-          <List 
+          <CommentForm post={post} />
+          <List
             header={`${post.Comments.length}개의 댓글`}
             itemLayout='horizontal'
             dataSource={post.Comments}
             renderItem={(item) => (
               <li>
-                <Comment post={post}
+                <Comment
+                  post={post}
                   author={item.User.nickName}
                   avatar={<Avatar>{item.User.nickName}</Avatar>}
                   content={item.content}
@@ -68,7 +109,7 @@ export default function PostCard({ post }) {
         </div>
       )}
     </div>
-  )
+  );
 }
 
 PostCard.prototype = {
@@ -79,5 +120,5 @@ PostCard.prototype = {
     createdAt: PropTypes.object,
     Commemts: PropTypes.arrayOf(PropTypes.object),
     Images: PropTypes.arrayOf(PropTypes.object),
-  }).isRequired
+  }).isRequired,
 };
