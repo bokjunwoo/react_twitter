85457698dc79d6f946ca3dc9812be2757f55cca9
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-16 23:25:53 +0900

    feat(1/16) : 코멘트폼 추가

--- front/componets/CommentForm.tsx
+++ front/componets/CommentForm.tsx
@@ -1,28 +1,48 @@
-import { Button, Form, Input } from 'antd'
-import React, { useCallback } from 'react'
-import userInput from '../hooks/userInput'
-import PropTypes from 'prop-types'
-import { useSelector } from 'react-redux'
+import { Button, Form, Input } from 'antd';
+import React, { useCallback, useEffect } from 'react';
+import userInput from '../hooks/userInput';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { ADD_COMMENT_REQUEST } from '../reducers/post';
 
 export default function CommentForm({ post }) {
-  const user = useSelector((state) => state.user.user?.id)
+  const dispatch = useDispatch();
 
-  const [commentText, onChangeCommentText] = userInput('')
+  const user = useSelector((state) => state.user.user?.id);
+  const { addCommentDone, addCommentLoding } = useSelector((state) => state.post);
+
+  const [commentText, onChangeCommentText, setCommentText] = userInput('');
+
+  useEffect(() => {
+    if (addCommentDone) {
+      setCommentText('');
+    }
+  }, [addCommentDone]);
 
   const onSubmitComment = useCallback(() => {
-    console.log(post.id, commentText)
-  }, [commentText])
+    console.log(post.id, commentText);
+    dispatch({
+      type: ADD_COMMENT_REQUEST,
+      data: { content: commentText, postId: post.id, userId: user },
+    });
+  }, [commentText, user]);
 
   return (
     <Form onFinish={onSubmitComment}>
       <Form.Item>
-        <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4} />
-        <Button type='primary' htmlType='submit'>삐약</Button>
+        <Input.TextArea
+          value={commentText}
+          onChange={onChangeCommentText}
+          rows={4}
+        />
+        <Button type='primary' htmlType='submit' style={{ position: 'absolute', right: 0, bottom: -40, zIndex: 1}} loading={addCommentLoding}>
+          삐약
+        </Button>
       </Form.Item>
     </Form>
-  )
+  );
 }
 
 CommentForm.prototype = {
-  post: PropTypes.object.isRequired
-}
+  post: PropTypes.object.isRequired,
+};
