7b1470b758cd5eec30d24724314fefdd2de3e1f0
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-22 02:16:01 +0900

    feat(1/22) : app.js 유저 라우터 연결

--- back/app.js
+++ back/app.js
@@ -1,6 +1,20 @@
-const express = require('express')
+const express = require('express');
+const db = require('./models');
 
-const app = express()
+const userRouter = require('./routers/user')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+db.sequelize.sync()
+  .then(() => {
+    console.log('DB 연결 성공')
+  })
+  .catch(console.error);
+
+app.use('user', userRouter)
 
 app.listen(3333, () => {
   console.log('서버 실행중')
