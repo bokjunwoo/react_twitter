aeb7d04e7b3e565fcc5381938c77446450128a6a
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-22 02:16:25 +0900

    feat(1/22) : 회원가입 백엔드 기능 구현

---
+++ back/routers/user.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bcrypt = require('bcrypt')
+const { User } = require('../models')
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    });
+    
+    if(exUser) {
+      return res.status(403).send('이미 사용중인 이메일 입니다.')
+    }
+    
+    const hashedPassword = await bcrypt.hash(req.body.pw, 12)
+    await User.create({
+      email: req.body.email,
+      ninkName: req.body.ninkName,
+      pw: hashedPassword,
+    });
+    res.status(201).send('ok');
+  }
+  catch {
+    console.error(error);
+    next(error); // status 500
+  }
+});
+
+module.exports = router;
