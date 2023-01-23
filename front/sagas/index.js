5b070f178b6af54535c7a2b74b42dd2c7b5a7747
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-23 22:25:31 +0900

    feat(1/23) : axios baseURL 설정

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -2,6 +2,9 @@ import { all, fork } from 'redux-saga/effects'
 // call(동기호출) fork(비동기호출)
 import userSaga from './user'
 import postSaga from './post'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3333';
 
 export default function* rootSaga() {
   yield all([ // all 동시에 실행
