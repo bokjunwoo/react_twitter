6c4348a104d1a8a77be9dd3c8f3a0653873ba516
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-24 20:03:33 +0900

    feat(1/24) : 포스트 받아오기 백 연결

---
+++ back/routers/posts.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { Post, User, Image, Comment } = require('../models')
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      limit: 10,
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC']
+      ],
+      include: [{
+        model: User,
+        attributes: ['id', 'nickName']
+      }, {
+        model: Image
+      }, {
+        model: Comment,
+        include: [{
+          model: User,
+          attributes: ['id', 'nickName']
+        }]
+      }]
+    });
+    res.status(200).json(posts);
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
