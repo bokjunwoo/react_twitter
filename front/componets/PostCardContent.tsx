b3d55f8fb3c0dfa296be6196ebeca0d208964ff6
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-28 00:57:58 +0900

    오타 수정

--- front/componets/PostCardContent.tsx
+++ front/componets/PostCardContent.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 export default function PostCardContent({ postData }) { // 첫번째 게시글 #해시태그
   return (
     <div>
-      {postData.split(/(#[^|s#]+)/g).map((v, i) => {
-        if (v.match(/(#[^|s#]+)/g)) {
+      {postData.split(/(#[^\s#]+)/g).map((v, i) => {
+        if (v.match(/(#[^\s#]+)/g)) {
           return <Link href={`/hashtag/${v.slice(1)}`} key={i}><a>{v}</a></Link>
         }
         return v
