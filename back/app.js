70f46893c5e61f6b42a1a374c6269449b4e90777
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-27 22:58:44 +0900

    multer 설치

--- back/app.js
+++ back/app.js
@@ -6,7 +6,8 @@ const session = require('express-session');
 const passport = require('passport')
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const path = require('path')
 
 dotenv.config();
 passportConfig();
@@ -18,6 +19,7 @@ app.use(cors({
   credentials: true // 쿠키도 허용
 }));
 
+app.use('/', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
