90ad26479b66d07a7bef73d15942c3af713bc46a
JUNU <bokjunwoo@gmail.com>
2023-02-12 23:46:11 +0900

    feat : 해쉬태그 수정 기능 추가

--- back/routers/post.js
+++ back/routers/post.js
@@ -1,278 +1,342 @@
-const express = require('express');
-const path = require('path')
-const multer = require('multer');
-const fs = require('fs')
-const { Post, Image, Comment, User, Hashtag } = require('../models');
-const { isLoggendIn } = require('./middlewares')
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
+const { Post, Image, Comment, User, Hashtag } = require("../models");
+const { isLoggendIn } = require("./middlewares");
 
 const router = express.Router();
 
-try{
-  fs.accessSync('uploads');
-} catch(error) {
-  console.log('폴더가 없으므로 생성합니다.')
-  fs.mkdirSync('uploads')
+try {
+  fs.accessSync("uploads");
+} catch (error) {
+  console.log("폴더가 없으므로 생성합니다.");
+  fs.mkdirSync("uploads");
 }
 
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
-      done(null, 'uploads');
+      done(null, "uploads");
     },
     filename(req, file, done) {
       const ext = path.extname(file.originalname); // 확장자 추출
-      const basename = path.basename(file.originalname, ext)
-      done(null, basename + '_' + new Date().getTime() + ext)
+      const basename = path.basename(file.originalname, ext);
+      done(null, basename + "_" + new Date().getTime() + ext);
     },
   }),
   limits: { fieldSize: 20 * 1024 * 1024 },
-})
+});
 
-router.post('/', isLoggendIn, upload.none(), async (req, res, next) => {
+router.post("/", isLoggendIn, upload.none(), async (req, res, next) => {
   try {
     const hashtag = req.body.content.match(/#[^\s#]+/g);
     const post = await Post.create({
       content: req.body.content,
-      UserId: req.user.id
+      UserId: req.user.id,
     });
-    if(hashtag) {
-      const result = await Promise.all(hashtag.map((tag) => Hashtag.findOrCreate({
-        where: {name: tag.slice(1).toLowerCase() },
-      }))); // ['노드', true], ['리액트', true]
-      await post.addHashtags(result.map((v) => v[0]))
+    if (hashtag) {
+      const result = await Promise.all(
+        hashtag.map((tag) =>
+          Hashtag.findOrCreate({
+            where: { name: tag.slice(1).toLowerCase() },
+          })
+        )
+      ); // ['노드', true], ['리액트', true]
+      await post.addHashtags(result.map((v) => v[0]));
     }
-    if(req.body.image) {
-      if(Array.isArray(req.body.image)) { // 이미지 여러 개 올리면 image: []
-        const images = await Promise.all(req.body.image.map((image) => Image.create({ src: image })));
-        await post.addImages(images)
-      } else { // 이미지 하나만 올리면 image : ''
+    if (req.body.image) {
+      if (Array.isArray(req.body.image)) {
+        // 이미지 여러 개 올리면 image: []
+        const images = await Promise.all(
+          req.body.image.map((image) => Image.create({ src: image }))
+        );
+        await post.addImages(images);
+      } else {
+        // 이미지 하나만 올리면 image : ''
         const image = await Image.create({ src: req.body.image });
-        await post.addImages(image)
+        await post.addImages(image);
       }
     }
     const fullPost = await Post.findOne({
       where: { id: post.id },
-      include: [{
-        model: Image,
-      }, {
-        model: Comment,
-        include: [{
-          model: User, // 댓글 작성자
-          attributes: ['id', 'nickName']
-        }]
-      }, {
-        model: User, // 게시글 작성자
-        attributes: ['id', 'nickName']
-      }, {
-        model: User, // 좋아요 누른 사람
-        as: 'Likers',
-        attributes: ['id', 'nickName']
-      }]
-    })
-    res.status(201).json(fullPost)
-  } catch(error) {
-    console.error(error)
-    next(error)
+      include: [
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User, // 댓글 작성자
+              attributes: ["id", "nickName"],
+            },
+          ],
+        },
+        {
+          model: User, // 게시글 작성자
+          attributes: ["id", "nickName"],
+        },
+        {
+          model: User, // 좋아요 누른 사람
+          as: "Likers",
+          attributes: ["id", "nickName"],
+        },
+      ],
+    });
+    res.status(201).json(fullPost);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
-router.post('/:postId/comment', isLoggendIn, async (req, res, next) => {
+router.post("/:postId/comment", isLoggendIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({
-      where: { id: req.params.postId }
-    })
-    if(!post) {
-      return res.status(403).send('존재하지 않는 게시글 입니다.')
+      where: { id: req.params.postId },
+    });
+    if (!post) {
+      return res.status(403).send("존재하지 않는 게시글 입니다.");
     }
     const comment = await Comment.create({
       content: req.body.content,
       PostId: parseInt(req.params.postId, 10),
-      UserId: req.user.id
+      UserId: req.user.id,
     });
     const fullComment = await Comment.findOne({
       where: { id: comment.id },
-      include: [{
-        model: User,
-        attributes: ['id', 'nickName']
-      }]
-    })
-    res.status(201).json(fullComment)
-  } catch(error) {
-    console.error(error)
-    next(error)
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickName"],
+        },
+      ],
+    });
+    res.status(201).json(fullComment);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
-router.patch('/:postId/like', isLoggendIn, async (req, res, next) => {
-  try{
-    const post = await Post.findOne({ where: { id: req.params.postId }});
-    if(!post) {
-      return res.status(403).send('게시글이 존재하지 않습니다');
-    };
+router.patch("/:postId/like", isLoggendIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({ where: { id: req.params.postId } });
+    if (!post) {
+      return res.status(403).send("게시글이 존재하지 않습니다");
+    }
     await post.addLikers(req.user.id);
-    res.status(201).json({ PostId: post.id, UserId: req.user.id})
-  } catch(error) {
+    res.status(201).json({ PostId: post.id, UserId: req.user.id });
+  } catch (error) {
     console.error(error);
     next(error);
   }
-})
+});
 
-router.patch('/:postId/unlike', isLoggendIn, async (req, res, next) => {
-  try{
-    const post = await Post.findOne({ where: { id: req.params.postId }});
-    if(!post) {
-      return res.status(403).send('게시글이 존재하지 않습니다');
-    };
+router.patch("/:postId/unlike", isLoggendIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({ where: { id: req.params.postId } });
+    if (!post) {
+      return res.status(403).send("게시글이 존재하지 않습니다");
+    }
     await post.removeLikers(req.user.id);
-    res.status(201).json({ PostId: post.id, UserId: req.user.id})
-  } catch(error) {
+    res.status(201).json({ PostId: post.id, UserId: req.user.id });
+  } catch (error) {
     console.error(error);
     next(error);
   }
-})
+});
 
-router.patch('/:postId', isLoggendIn, async (req, res, next) => {
-  try{
-    await Post.update({
-      content: req.body.content 
-    }, {
-      where: { 
-        id: req.params.postId,
-        UserId: req.user.id,
+router.patch("/:postId", isLoggendIn, async (req, res, next) => {
+  const hashtag = req.body.content.match(/#[^\s#]+/g);
+  try {
+    await Post.update(
+      {
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.postId,
+          UserId: req.user.id,
+        },
       }
-    })
-    res.status(200).json({ PostId: parseInt(req.params.postId, 10), content: req.body.content });
-  } catch(error) {
+    );
+    const post = await Post.findOne({ where: { id: req.params.postId } });
+    if (hashtag) {
+      const result = await Promise.all(
+        hashtag.map((tag) =>
+          Hashtag.findOrCreate({
+            where: { name: tag.slice(1).toLowerCase() },
+          })
+        )
+      ); // ['노드', true], ['리액트', true]
+      await post.setHashtags(result.map((v) => v[0]));
+    }
+    res
+      .status(200)
+      .json({
+        PostId: parseInt(req.params.postId, 10),
+        content: req.body.content,
+      });
+  } catch (error) {
     console.error(error);
     next(error);
   }
-})
+});
 
-router.delete('/:postId', isLoggendIn, async (req, res, next) => {
-  try{
+router.delete("/:postId", isLoggendIn, async (req, res, next) => {
+  try {
     await Post.destroy({
-      where: { 
+      where: {
         id: req.params.postId,
         UserId: req.user.id,
-      }
-    })
+      },
+    });
     res.status(200).json({ PostId: parseInt(req.params.postId, 10) });
-  } catch(error) {
+  } catch (error) {
     console.error(error);
     next(error);
   }
-})
+});
 
-router.post('/images', isLoggendIn, upload.array('image'), (req, res, next) => {
+router.post("/images", isLoggendIn, upload.array("image"), (req, res, next) => {
   console.log(req.files);
   res.json(req.files.map((v) => v.filename));
-})
+});
 
-router.post('/:postId/retweet', isLoggendIn, async (req, res, next) => {
+router.post("/:postId/retweet", isLoggendIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({
       where: { id: req.params.postId },
-      include: [{
-        model: Post,
-        as: 'Retweet',
-      }]
-    })
-    if(!post) {
-      return res.status(403).send('존재하지 않는 게시글 입니다.')
+      include: [
+        {
+          model: Post,
+          as: "Retweet",
+        },
+      ],
+    });
+    if (!post) {
+      return res.status(403).send("존재하지 않는 게시글 입니다.");
     }
-    if(req.user.id === post.UserId || (post.Retweet && post.Retweet.UserId === req.user.id)) {
-      return res.status(403).send('자신의 글은 리트윗 할 수 없습니다');
-    } 
+    if (
+      req.user.id === post.UserId ||
+      (post.Retweet && post.Retweet.UserId === req.user.id)
+    ) {
+      return res.status(403).send("자신의 글은 리트윗 할 수 없습니다");
+    }
     const retweetTarget = post.RetweetId || post.id;
     const exPost = await Post.findOne({
       where: {
         UserId: req.user.id,
         RetweetId: retweetTarget,
-      }
+      },
     });
-    if(exPost) {
-      return res.status(403).send('이미 리트윗 했습니다');
+    if (exPost) {
+      return res.status(403).send("이미 리트윗 했습니다");
     }
     const retweet = await Post.create({
       UserId: req.user.id,
       RetweetId: retweetTarget,
-      content: 'retweet'
-    })
+      content: "retweet",
+    });
     const retweetWithPrevPost = await Post.findOne({
       where: { id: retweet.id },
-      include: [{
-        model: Post,
-        as: 'Retweet',
-        include: [{
+      include: [
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickName"],
+            },
+            {
+              model: Image,
+            },
+          ],
+        },
+        {
           model: User,
-          attributes: ['id', 'nickName'],
-        }, {
+          attributes: ["id", "nickName"],
+        },
+        {
           model: Image,
-        }]
-      }, {
-        model: User,
-        attributes: ['id', 'nickName'],
-      }, {
-        model: Image,
-      }, {
-        model: Comment,
-        include: [{
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickName"],
+            },
+          ],
+        },
+        {
           model: User,
-          attributes: ['id', 'nickName'],
-        }],
-      }, {
-        model: User,
-        as: 'Likers',
-        attributes: ['id']
-      }],
-    })
-    res.status(201).json(retweetWithPrevPost)
-  } catch(error) {
-    console.error(error)
-    next(error)
+          as: "Likers",
+          attributes: ["id"],
+        },
+      ],
+    });
+    res.status(201).json(retweetWithPrevPost);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
-router.get('/:postId', async (req, res, next) => {
+router.get("/:postId", async (req, res, next) => {
   try {
     const post = await Post.findOne({
       where: { id: req.params.postId },
-    })
-    if(!post) {
-      return res.status(404).send('존재하지 않는 게시글 입니다.')
+    });
+    if (!post) {
+      return res.status(404).send("존재하지 않는 게시글 입니다.");
     }
     const fullPost = await Post.findOne({
       where: { id: post.id },
-      include: [{
-        model: Post,
-        as: 'Retweet',
-        include: [{
+      include: [
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickName"],
+            },
+            {
+              model: Image,
+            },
+          ],
+        },
+        {
           model: User,
-          attributes: ['id', 'nickName'],
-        }, {
+          attributes: ["id", "nickName"],
+        },
+        {
           model: Image,
-        }]
-      }, {
-        model: User,
-        attributes: ['id', 'nickName'],
-      }, {
-        model: Image,
-      }, {
-        model: Comment,
-        include: [{
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickName"],
+            },
+          ],
+        },
+        {
           model: User,
-          attributes: ['id', 'nickName'],
-        }],
-      }, {
-        model: User,
-        as: 'Likers',
-        attributes: ['id']
-      }],
-    })
-    res.status(200).json(fullPost)
-  } catch(error) {
-    console.error(error)
-    next(error)
+          as: "Likers",
+          attributes: ["id"],
+        },
+      ],
+    });
+    res.status(200).json(fullPost);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
